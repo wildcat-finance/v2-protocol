829c3180e4c9b94e173ac2ea87b9d402596386a1
d1ll0n <dillon@indexed.finance>
2024-03-19 13:26:36 -0700

    update types

---
+++ src/types/TypeCasts.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+import '../interfaces/WildcatStructsAndEnums.sol';
+import '../libraries/MarketState.sol';
+import '../libraries/Withdrawal.sol';
+
+library TypeCasts {
+  /**
+   * @dev Function type cast to avoid duplicate declaration of Account return parameter.
+   *
+   *      With `viaIR` enabled, calling this function is a noop.
+   */
+  function asReturnsAccount(
+    function(address, AuthRole) internal returns (uint256) fnIn
+  ) internal pure returns (function(address, AuthRole) internal returns (Account memory) fnOut) {
+    assembly {
+      fnOut := fnIn
+    }
+  }
+
+  /**
+   * @dev Function type cast to avoid duplicate declaration of MarketState return parameter.
+   *
+   *      With `viaIR` enabled, calling this function is a noop.
+   */
+  function asReturnsMarketState(
+    function() internal view returns (uint256) fnIn
+  ) internal pure returns (function() internal view returns (MarketState memory) fnOut) {
+    assembly {
+      fnOut := fnIn
+    }
+  }
+
+  /**
+   * @dev Function type cast to avoid duplicate declaration of MarketState and WithdrawalBatch
+   *      return parameters.
+   *
+   *      With `viaIR` enabled, calling this function is a noop.
+   */
+  function asReturnsStateWithExpiryAndBatch(
+    function() internal view returns (MarketState memory, uint32, WithdrawalBatch memory) fnIn
+  ) internal pure returns (function() internal view returns (uint256, uint32, uint256) fnOut) {
+    assembly {
+      fnOut := fnIn
+    }
+  }
+}
