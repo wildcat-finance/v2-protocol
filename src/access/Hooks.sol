0194739390379d8c882b80905b78a083c627012b
d1ll0n <dillon@indexed.finance>
2024-03-24 09:01:33 -0700

    Add some additional logic for handling credentials context in a call, separate type libraries

--- src/access/Hooks.sol
+++ src/access/Hooks.sol
@@ -4,24 +4,11 @@ pragma solidity ^0.8.20;
 import '../libraries/BoolUtils.sol';
 import '../libraries/MathUtils.sol';
 import '../types/RoleProvider.sol';
+import '../types/LenderStatus.sol';
+import './IRoleProvider.sol';
 
 using BoolUtils for bool;
 using MathUtils for uint256;
-
-/**
- * @param isBlocked Whether the lender is blocked from the market
- * @param hasEverDeposited Whether the lender has ever deposited to the market
- * @param lastProvider The address of the last provider to grant the lender a credential
- * @param canRefresh Whether the last provider can refresh the lender's credential
- * @param expiry The timestamp at which the lender's credential expires
- */
-struct LenderStatus {
-  bool isBlocked;
-  bool hasEverDeposited;
-  address lastProvider;
-  bool canRefresh;
-  uint32 expiry;
-}
 
 function removeFromArray(
   RoleProvider[] storage arr,
@@ -42,14 +29,8 @@ function removeFromArray(
   map[lastProvider.providerAddress()] = lastProvider;
 }
 
-interface IRoleProvider {
-  function canPull() external view returns (bool);
-
-  function getCredential(address account) external view returns (uint32 timestamp);
-
-  function validateCredential(address account) external returns (bytes4 magicValue);
-}
-
+// @todo custom error types
+// @todo events
 contract AccessControlHooks {
   uint32 providerCount;
   mapping(address => LenderStatus) internal _lenderStatus;
@@ -57,32 +38,10 @@ contract AccessControlHooks {
   // push providers to update in a single step and pull providers to
   // be looped over without having to access the mapping.
   RoleProvider[] internal pullProviders;
-  mapping(address => RoleProvider) internal roleProviders;
-
-  function grantRole(address account) external {
-    RoleProvider provider = roleProviders[msg.sender];
-    require(!provider.isNull(), 'Controller: Not a push provider');
-    LenderStatus memory status = _lenderStatus[account];
-    if (status.expiry > 0) {
-      // Can only update role if it is expired or caller is previous role provider
-      bool isPreviousRoleExpired = status.expiry < block.timestamp;
-      bool isPreviousRoleProvider = status.lastProvider == msg.sender;
-      require(
-        isPreviousRoleExpired.or(isPreviousRoleProvider),
-        'Role is not expired and sender is not previous role provider'
-      );
-    }
-    uint256 timeToLive = provider.timeToLive();
-    // todo - overflow possible in third argument - fix
-    uint256 newExpiry = block.timestamp.satAdd(timeToLive, type(uint32).max);
-    status.expiry = uint32(newExpiry);
-    status.lastProvider = msg.sender;
-    status.canRefresh = provider.isPullProvider();
-    _lenderStatus[account] = status;
-  }
+  mapping(address => RoleProvider) internal _roleProviders;
 
   function addProvider(address providerAddress, bool isPullProvider, uint32 timeToLive) external {
-    RoleProvider provider = roleProviders[providerAddress];
+    RoleProvider provider = _roleProviders[providerAddress];
     if (!provider.isNull()) {
       // If provider already exists, the only value that can be updated is the TTL
       provider = provider.setTimeToLive(timeToLive);
@@ -97,32 +56,34 @@ contract AccessControlHooks {
       if (isPullProvider) pullProviders.push(provider);
     }
     // Update the provider in storage
-    roleProviders[providerAddress] = provider;
+    _roleProviders[providerAddress] = provider;
   }
 
-  // function _refresh(
-  //   LenderStatus memory status,
-  //   RoleProvider memory provider,
-  //   uint32 providerId,
-  //   address accountAddress
-  // ) internal returns (bool /* refreshed */) {
-  //   // todo - query in assembly, return false if call fails
-  //   uint256 credentialTimestamp = provider.provider.getCredential(accountAddress);
+  function removeProvider(address providerAddress) external {
+    RoleProvider provider = _roleProviders[providerAddress];
+    require(!provider.isNull(), 'Provider null');
+    if (provider.isPullProvider()) {
+      removeFromArray(pullProviders, _roleProviders, provider.pullProviderIndex());
+    }
+    delete _roleProviders[providerAddress];
+  }
 
-  //   if (credentialTimestamp == 0) return false;
-
-  //   // Calculate new role expiry as either max uint32 or the last approval time
-  //   // plus the provider's TTL, whichever is lower.
-  //   uint256 newExpiry = credentialTimestamp.satAdd(provider.timeToLive(), type(uint32).max);
-
-  //   // If new expiry would be expired, do not update status
-  //   if (newExpiry > block.timestamp) {
-  //     // User is approved, update status with new expiry and last provider
-  //     status.expiry = uint32(newExpiry);
-  //     status.lastProviderId = providerId;
-  //     return true;
-  //   }
-  // }
+  function grantRole(address account) external {
+    RoleProvider provider = _roleProviders[msg.sender];
+    require(!provider.isNull(), 'Controller: Not a push provider');
+    LenderStatus memory status = _lenderStatus[account];
+    if (status.expiry > 0) {
+      // Can only update role if it is expired or caller is previous role provider
+      bool isPreviousRoleExpired = status.expiry < block.timestamp;
+      bool isPreviousRoleProvider = status.lastProvider == msg.sender;
+      require(
+        isPreviousRoleExpired.or(isPreviousRoleProvider),
+        'Role is not expired and sender is not previous role provider'
+      );
+    }
+    status.setCredential(provider, block.timestamp);
+    _lenderStatus[account] = status;
+  }
 
   function tryPullUserAccess(
     LenderStatus memory status,
@@ -140,19 +101,48 @@ contract AccessControlHooks {
 
     if (lastApprovalTime == 0) return false;
 
-    uint256 ttl = provider.timeToLive();
     // Calculate new role expiry as either max uint32 or the last approval time
     // plus the provider's TTL, whichever is lower.
-    uint256 newExpiry = lastApprovalTime.satAdd(ttl, type(uint32).max);
+    uint256 newExpiry = lastApprovalTime.satAdd(provider.timeToLive(), type(uint32).max);
 
     // If new expiry would be expired, do not update status
-    if (newExpiry > block.timestamp) {
-      // User is approved, update status with new expiry and last provider
-      status.expiry = uint32(newExpiry);
-      status.lastProvider = address(roleProvider);
-      status.canRefresh = true;
-      return true;
+    if (newExpiry < block.timestamp) return false;
+
+    // User is approved, update status with new expiry and last provider
+    status.setCredential(provider, lastApprovalTime);
+    return true;
+  }
+
+  // @todo update ethereum-access-token to allow the provider to specify the
+  //       cdptr to the signature
+  function _validateCredential(address accountAddress, uint calldataPointer) internal {
+    uint validateSelector = IRoleProvider.validateCredential.selector;
+    address providerAddress;
+    assembly {
+      providerAddress := shr(96, calldataload(calldataPointer))
+      // Increment calldata pointer forward 20 bytes to indicate the remainder is
+      // the data to pass to the provider
+      calldataPointer := add(calldataPointer, 0x14)
     }
+    RoleProvider provider = _roleProviders[providerAddress];
+    // @todo custom error
+    require(!provider.isNull(), 'Provider not found');
+    assembly {
+      let ptr := mload(0x40)
+      mstore(ptr, validateSelector)
+      mstore(add(ptr, 0x20), accountAddress)
+      let extraCalldataBytes := sub(calldatasize(), calldataPointer)
+      calldatacopy(add(ptr, 0x40), calldataPointer, extraCalldataBytes)
+      let size := add(0x24, extraCalldataBytes)
+      if iszero(
+        and(eq(mload(0), selector), call(gas(), sload(accountAddress.slot), 0, ptr, size, 0, 0))
+      ) {
+        returndatacopy(0, 0, returndatasize())
+        revert(0, returndatasize())
+      }
+    }
+    LenderStatus memory status = _lenderStatus[accountAddress];
+    status.setCredential(provider, block.timestamp, type(uint32).max);
   }
 
   /**
@@ -171,32 +161,34 @@ contract AccessControlHooks {
   ) external view returns (LenderStatus memory status) {
     status = _lenderStatus[accountAddress];
 
-    uint32 providerIndexToSkip;
+    uint256 providerIndexToSkip;
+
     // Check if user has an existing credential
     if (status.expiry != 0) {
-      // If credential is not expired and the provider is still supported,
-      // return the status.
-      if (status.expiry > block.timestamp) {
-        RoleProvider provider = roleProviders[status.lastProvider];
-        if (!provider.isNull()) return status;
-      }
-      // If user's role is expired, check if it can be refreshed
-      if ((status.expiry < block.timestamp).and(status.canRefresh)) {
-        RoleProvider provider = roleProviders[status.lastProvider];
-        // Verify the provider is still supported
+      // Check if the credential is still valid
+      if (status.expiry >= block.timestamp) {
+        // If credential is not expired and the provider is still
+        // supported, the lender has a valid credential.
+        RoleProvider provider = _roleProviders[status.lastProvider];
+        if (!provider.isNull()) {
+          return status;
+        }
+      } else if (status.canRefresh) {
+        // If credential is expired but the provider is still supported and
+        // allows refreshing (i.e. it's a pull provider), try to refresh.
+        RoleProvider provider = _roleProviders[status.lastProvider];
         if (!provider.isNull()) {
           // If the credential is refreshed, return the status
           if (tryPullUserAccess(status, provider, accountAddress)) {
             return status;
           }
           // Otherwise, skip the provider on the loop below
+          // Don't need to set a skip index if provider is null
+          // as it won't be in `pullProviders`
           providerIndexToSkip = provider.pullProviderIndex();
         }
-        status.canRefresh = false;
-        status.expiry = 0;
-        status.lastProvider = address(0);
-      } else {
-        return status;
+        // If credential could not be refreshed, remove it from the lender
+        status.unsetCredential();
       }
     }
     // Loop over all pull providers to find a valid role
