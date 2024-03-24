c0e1c3f27eea9595d1cf410669c1f296a950d6d4
d1ll0n <dillon@indexed.finance>
2024-03-24 09:04:00 -0700

    rm account

--- src/types/Account.sol
+++ src/types/Account.sol
@@ -1,6 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
 struct Account {
-    bool hasEverDeposited;
-    bool isAuthorized;
-    uint32 expiry;
-    uint104 scaledBalance;
+  bool hasEverDeposited;
+  bool isAuthorized;
+  uint32 expiry;
+  uint104 scaledBalance;
 }
