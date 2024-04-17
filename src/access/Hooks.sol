7fbf562e3868953186ae2e5a284169e58a2d8254
d1ll0n <dillon@indexed.finance>
2024-04-17 03:36:01 -0700

    events, errors, general refactor

--- src/access/Hooks.sol
+++ src/access/Hooks.sol
@@ -11,25 +11,6 @@ import './IHooks.sol';
 using BoolUtils for bool;
 using MathUtils for uint256;
 
-function removeFromArray(
-  RoleProvider[] storage arr,
-  mapping(address => RoleProvider) storage map,
-  uint24 indexToRemove
-) {
-  // Get the last index in the array
-  uint256 lastIndex = arr.length - 1;
-  // If the index to remove is the last index, just pop the last element
-  if (indexToRemove == lastIndex) {
-    arr.pop();
-    return;
-  }
-  RoleProvider lastProvider = arr[lastIndex];
-  lastProvider = lastProvider.setPullProviderIndex(indexToRemove);
-  arr[indexToRemove] = lastProvider;
-  arr.pop();
-  map[lastProvider.providerAddress()] = lastProvider;
-}
-
 // @todo custom error types
 // @todo events
 
@@ -46,6 +27,37 @@ function removeFromArray(
  *      Deposit access may be canceled by the borrower.
  */
 contract AccessControlHooks is IHooks {
+  event RoleProviderUpdated(
+    address indexed providerAddress,
+    uint32 timeToLive,
+    uint24 pullProviderIndex
+  );
+  event RoleProviderAdded(
+    address indexed providerAddress,
+    uint32 timeToLive,
+    uint24 pullProviderIndex
+  );
+  event RoleProviderRemoved(address indexed providerAddress, uint24 pullProviderIndex);
+  event AccountBlockedFromDeposits(address indexed accountAddress);
+  event AccountUnblockedFromDeposits(address indexed accountAddress);
+  event AccountAccessGranted(
+    address indexed providerAddress,
+    address indexed accountAddress,
+    uint32 credentialTimestamp
+  );
+
+  error ProviderNotFound();
+  error ProviderCanNotReplaceCredential();
+  /// @dev Error thrown when a provider grants a credential that is already expired.
+  error GrantedCredentialExpired();
+
+  mapping(address => LenderStatus) internal _lenderStatus;
+  // Provider data is duplicated in the array and mapping to allow
+  // push providers to update in a single step and pull providers to
+  // be looped over without having to access the mapping.
+  RoleProvider[] internal _pullProviders;
+  mapping(address => RoleProvider) internal _roleProviders;
+
   function version() external pure override returns (string memory) {
     return 'AccessControlHooks';
   }
@@ -67,20 +79,13 @@ contract AccessControlHooks is IHooks {
       });
   }
 
-  mapping(address => LenderStatus) internal _lenderStatus;
-  // Provider data is duplicated in the array and mapping to allow
-  // push providers to update in a single step and pull providers to
-  // be looped over without having to access the mapping.
-  RoleProvider[] internal _pullProviders;
-  mapping(address => RoleProvider) internal _roleProviders;
-
   /**
    * @dev Adds or updates a role provider that is able to grant user access.
    *      If it is not already approved, it is added to `_roleProviders` and,
    *      if the provider can refresh credentials, added to `pullProviders`.
    *      If the provider is already approved, only updates `timeToLive`.
    */
-  function addProvider(address providerAddress, uint32 timeToLive) external {
+  function addRoleProvider(address providerAddress, uint32 timeToLive) external {
     RoleProvider provider = _roleProviders[providerAddress];
     if (provider.isNull()) {
       bool isPullProvider = IRoleProvider(providerAddress).isPullProvider();
@@ -94,80 +99,141 @@ contract AccessControlHooks is IHooks {
       if (isPullProvider) {
         _pullProviders.push(provider);
       }
+      emit RoleProviderAdded(providerAddress, timeToLive, provider.pullProviderIndex());
     } else {
       // If provider already exists, the only value that can be updated is the TTL
       provider = provider.setTimeToLive(timeToLive);
+      uint24 pullProviderIndex = provider.pullProviderIndex();
+      if (pullProviderIndex != NotPullProviderIndex) {
+        _pullProviders[pullProviderIndex] = provider;
+      }
+      emit RoleProviderUpdated(providerAddress, timeToLive, pullProviderIndex);
     }
     // Update the provider in storage
     _roleProviders[providerAddress] = provider;
   }
 
-  function removeProvider(address providerAddress) external {
-    RoleProvider provider = _roleProviders[providerAddress];
-    // @todo custom error
-    require(!provider.isNull(), 'Provider null');
-    if (provider.isPullProvider()) {
-      removeFromArray(_pullProviders, _roleProviders, provider.pullProviderIndex());
-    }
-    _roleProviders[providerAddress] = EmptyRoleProvider;
+  function getRoleProvider(address providerAddress) external view returns (RoleProvider) {
+    return _roleProviders[providerAddress];
   }
 
-  function grantRole(address account) external {
-    RoleProvider callingProvider = _roleProviders[msg.sender];
-    require(!callingProvider.isNull(), 'Controller: Not a push provider');
-    LenderStatus memory status = _lenderStatus[account];
-    if (status.lastApprovalTimestamp > 0) {
-      RoleProvider lastProvider = _roleProviders[status.lastProvider];
-      if (!lastProvider.isNull()) {
-        // Can only update role if it is expired or caller is previous role provider
-        bool isPreviousRoleExpired = status.hasExpiredCredential(lastProvider);
-        bool isPreviousRoleProvider = status.lastProvider == msg.sender;
-        require(
-          isPreviousRoleExpired.or(isPreviousRoleProvider),
-          'Role is not expired and sender is not previous role provider'
-        );
-      }
+  function getPullProviders() external view returns (RoleProvider[] memory) {
+    return _pullProviders;
+  }
+
+  function removeRoleProvider(address providerAddress) external {
+    RoleProvider provider = _roleProviders[providerAddress];
+    if (provider.isNull()) revert ProviderNotFound();
+    // Remove the provider from `_roleProviders`
+    _roleProviders[providerAddress] = EmptyRoleProvider;
+    emit RoleProviderRemoved(providerAddress, provider.pullProviderIndex());
+    // If the provider is a pull provider, remove it from `_pullProviders`
+    if (provider.isPullProvider()) {
+      _removePullProvider(provider.pullProviderIndex());
     }
-    status.setCredential(callingProvider, block.timestamp);
-    _lenderStatus[account] = status;
   }
 
   /**
-   * @dev Tries to pull an active credential for an account from a
-   *      pull provider. If one exists, updates the account in memory
-   *      and returns true.
+   * @dev Remove a pull provider from the `_pullProviders` array.
+   *      If the provider is not the last in the array, the last provider
+   *      is moved to the index of the provider being removed, so its index
+   *      must also be updated in the `_roleProviders` mapping.
+   */
+  function _removePullProvider(uint24 indexToRemove) internal {
+    // Get the last index in the array
+    uint256 lastIndex = _pullProviders.length - 1;
+    // If the index to remove is the last index, just pop the last element
+    if (indexToRemove == lastIndex) {
+      _pullProviders.pop();
+      return;
+    }
+    // If the index to remove is not the last index, move the last element
+    // to the index of the element being removed
+    RoleProvider lastProvider = _pullProviders[lastIndex].setPullProviderIndex(indexToRemove);
+    _pullProviders[indexToRemove] = lastProvider;
+    _pullProviders.pop();
+    address lastProviderAddress = lastProvider.providerAddress();
+    _roleProviders[lastProviderAddress] = lastProvider;
+    // Emit an event to notify that the provider's index has been updated
+    emit RoleProviderUpdated(lastProviderAddress, lastProvider.timeToLive(), indexToRemove);
+  }
+
+  /**
+   * @dev Grants a role to an account by updating the account's status.
+   *      Can only be called by an approved role provider.
+   *
+   *      If the account has an existing credential, it can only be updated if:
+   *      - the previous credential's provider is no longer supported, OR
+   *      - the caller is the previous role provider, OR
+   *      - the new expiry is later than the current expiry
+   */
+  function grantRole(address account, uint32 roleGrantedTimestamp) external {
+    RoleProvider callingProvider = _roleProviders[msg.sender];
+
+    if (callingProvider.isNull()) revert ProviderNotFound();
+
+    LenderStatus memory status = _lenderStatus[account];
+
+    uint256 newExpiry = callingProvider.calculateExpiry(roleGrantedTimestamp);
+
+    // Check if the new credential is still valid
+    if (newExpiry < block.timestamp) revert GrantedCredentialExpired();
+
+    // Check if the account has ever had a credential
+    if (status.lastApprovalTimestamp > 0) {
+      RoleProvider lastProvider = _roleProviders[status.lastProvider];
+
+      // Check if the provider that last granted access is still supported
+      if (!lastProvider.isNull()) {
+        uint256 oldExpiry = lastProvider.calculateExpiry(status.lastApprovalTimestamp);
+
+        // Can only update role if the caller is the previous role provider or the new
+        // expiry is greater than the previous expiry.
+        if (!((status.lastProvider == msg.sender).or(newExpiry > oldExpiry))) {
+          revert ProviderCanNotReplaceCredential();
+        }
+      }
+    }
+
+    _setCredentialAndEmitAccessGranted(status, callingProvider, account, roleGrantedTimestamp);
+  }
+
+  /**
+   * @dev Tries to pull an active credential for an account from a pull provider.
+   *      If one exists, updates the account in memory and returns true.
    *
    *      Note: Does not check that provider is a pull provider - should
    *      only be called if that has already been checked.
    */
-  function _tryPullCredential(
+  function _tryGetCredential(
     LenderStatus memory status,
     RoleProvider provider,
     address accountAddress
   ) internal view returns (bool isApproved) {
     // Query provider for user approval
-    IRoleProvider roleProvider = IRoleProvider(provider.providerAddress());
+    address providerAddress = provider.providerAddress();
 
-    // todo - query in assembly, return false if call fails
-    uint256 lastApprovalTime = roleProvider.getCredential(accountAddress);
-
-    if (lastApprovalTime > 0) {
-      // Calculate new role expiry as either max uint32 or the last approval time
-      // plus the provider's TTL, whichever is lower.
-      uint256 newExpiry = provider.calculateExpiry(lastApprovalTime);
-
-      // If credential is still valid, update credential
-      if (newExpiry >= block.timestamp) {
-        // User is approved, update status with new expiry and last provider
-        status.setCredential(provider, lastApprovalTime);
-        return true;
+    uint32 lastApprovalTime;
+    uint getCredentialSelector = uint32(IRoleProvider.getCredential.selector);
+    assembly {
+      mstore(0x00, getCredentialSelector)
+      mstore(0x20, accountAddress)
+      // Call the provider and check if the return data is valid
+      if and(gt(returndatasize(), 0x1f), staticcall(gas(), providerAddress, 0x1c, 0x24, 0, 0x20)) {
+        // If the return data is valid, set `lastApprovalTime` to the returned word
+        // with a uint32 mask applied
+        lastApprovalTime := and(mload(0), 0xffffffff)
       }
+    }
+
+    // If credential is still valid, update credential
+    if (lastApprovalTime > 0 && provider.calculateExpiry(lastApprovalTime) >= block.timestamp) {
+      // User is approved, update status with new expiry and last provider
+      status.setCredential(provider, lastApprovalTime);
+      return true;
     }
     return false;
   }
-
-  // @todo update ethereum-access-token to allow the provider to specify the
-  //       cdptr to the signature
 
   function _readProviderAddressFromCalldataSuffix(
     uint256 baseCalldataSize
@@ -197,6 +263,7 @@ contract AccessControlHooks is IHooks {
     address accountAddress,
     uint256 baseCalldataSize
   ) internal returns (bool) {
+    // @todo use constant selector once interface is fixed
     uint validateSelector = uint32(IRoleProvider.validateCredential.selector);
     address providerAddress = _readProviderAddressFromCalldataSuffix(baseCalldataSize);
     RoleProvider provider = _roleProviders[providerAddress];
@@ -257,7 +324,7 @@ contract AccessControlHooks is IHooks {
   ) external view returns (LenderStatus memory status) {
     status = _lenderStatus[accountAddress];
 
-    uint256 providerIndexToSkip;
+    uint256 pullProviderIndexToSkip;
 
     // Check if user has an existing credential
     if (status.lastApprovalTimestamp > 0) {
@@ -270,11 +337,11 @@ contract AccessControlHooks is IHooks {
         // If credential is expired but the provider is still supported and
         // allows refreshing (i.e. it's a pull provider), try to refresh.
         if (status.canRefresh) {
-          if (_tryPullCredential(status, provider, accountAddress)) {
+          if (_tryGetCredential(status, provider, accountAddress)) {
             return status;
           }
           // If refresh fails, provider should be skipped in the query loop
-          providerIndexToSkip = provider.pullProviderIndex();
+          pullProviderIndexToSkip = provider.pullProviderIndex();
         }
       }
       // If credential could not be refreshed or the provider is no longer
@@ -282,15 +349,108 @@ contract AccessControlHooks is IHooks {
       status.unsetCredential();
     }
 
-    uint256 providerCount = _pullProviders.length;
     // Loop over all pull providers to find a valid role for the lender
-    for (uint256 i = 0; i < providerCount; i++) {
-      if (i == providerIndexToSkip) continue;
-      RoleProvider provider = _pullProviders[i];
-      if (_tryPullCredential(status, provider, accountAddress)) {
-        return status;
-      }
+    if (_loopTryGetCredential(status, accountAddress, pullProviderIndexToSkip)) {
+      return status;
     }
+  }
+
+  function _checkCalldataSuffix(
+    uint256 baseCalldataSize
+  ) internal pure returns (uint256 suffixSize) {
+    assembly {
+      suffixSize := sub(calldatasize(), baseCalldataSize)
+    }
+  }
+
+  /// @dev Loops over all pull providers to find a valid credential for the lender.
+  function _loopTryGetCredential(
+    LenderStatus memory status,
+    address accountAddress,
+    uint256 pullProviderIndexToSkip
+  ) internal view returns (bool) {
+    uint256 providerCount = _pullProviders.length;
+    for (uint256 i = 0; i < providerCount; i++) {
+      if (i == pullProviderIndexToSkip) continue;
+      RoleProvider provider = _pullProviders[i];
+      if (_tryGetCredential(status, provider, accountAddress)) return (true);
+    }
+  }
+
+  function _handleCalldataSuffix(
+    LenderStatus memory status,
+    address accountAddress,
+    uint256 baseCalldataSize
+  ) internal returns (bool validCredential) {
+    // Check if a calldata suffix was provided
+    uint256 suffixSize = _checkCalldataSuffix(baseCalldataSize);
+    if (suffixSize == 20) {
+      // @todo make the methods of updating based on the cd prefix more consistent
+
+      // If the suffix contains only an address, attempt to query a credential from that provider
+      address providerAddress = _readProviderAddressFromCalldataSuffix(baseCalldataSize);
+      RoleProvider provider = _roleProviders[providerAddress];
+      if (!provider.isNull() && _tryGetCredential(status, provider, accountAddress)) return true;
+    } else if (suffixSize > 20) {
+      // If the suffix contains both an address and raw data, attempt to validate a credential from that provider
+      if (_tryValidateCredential(status, accountAddress, baseCalldataSize)) return true;
+    }
+  }
+
+  /**
+   * @dev Internal function used to validate or update the status of a lender account for hooks on restricted actions.
+   *
+   *     The function follows the following logic, with the process ending if a valid credential is found:
+   *       1. Check if lender has an existing unexpired credential.
+   *       2. Check if a calldata suffix was provided, and if so:
+   *         - If the suffix contains only an address, attempt to query a credential from that provider.
+   *         - If the suffix contains both an address and raw data, attempt to validate a credential from that provider.
+   *       3. If lender has an existing expired credential, attempt to refresh it.
+   *       4. Loop over all pull providers to find a valid credential, excluding the last provider if it failed to refresh.
+   *
+   * note: Does not update storage or emit an event, but is stateful because it can invoke `validateCredential` on a provider.
+   */
+  function _tryValidateOrUpdateStatus(
+    LenderStatus memory status,
+    address accountAddress,
+    uint256 baseCalldataSize
+  ) internal returns (bool hasValidCredential, bool wasUpdated) {
+    status = _lenderStatus[accountAddress];
+    // Get the last provider that granted the lender a credential, if any
+    RoleProvider lastProvider = status.hasCredential()
+      ? _roleProviders[status.lastProvider]
+      : EmptyRoleProvider;
+
+    // If the lender has an active credential and the last provider is still supported, return
+    if (!lastProvider.isNull() && status.hasActiveCredential(lastProvider)) return (true, false);
+
+    // Handle the calldata suffix, if any
+    if (_handleCalldataSuffix(status, accountAddress, baseCalldataSize)) return (true, false);
+
+    uint256 pullProviderIndexToSkip;
+
+    // If lender has an expired credential from a pull provider, attempt to refresh it
+    if (!lastProvider.isNull() && status.canRefresh) {
+      if (_tryGetCredential(status, lastProvider, accountAddress)) return (true, true);
+      // If refresh fails, provider should be skipped in the query loop
+      pullProviderIndexToSkip = lastProvider.pullProviderIndex();
+    }
+
+    // Loop over all pull providers to find a valid role for the lender
+    if (_loopTryGetCredential(status, accountAddress, pullProviderIndexToSkip)) return (true, true);
+  }
+
+  function _setCredentialAndEmitAccessGranted(
+    LenderStatus memory status,
+    RoleProvider provider,
+    address accountAddress,
+    uint32 credentialTimestamp
+  ) internal {
+    // Update the account's status with the new credential in memory
+    status.setCredential(provider, credentialTimestamp);
+    // Update the account's status in storage
+    _lenderStatus[accountAddress] = status;
+    emit AccountAccessGranted(provider.providerAddress(), accountAddress, credentialTimestamp);
   }
 
   function onDeposit(
