a5af91d65f930b5924b0cb8bf1d470cbd9a1bbb8
d1ll0n <dillon@indexed.finance>
2024-04-26 14:13:52 -0700

    tests passing

--- test/shared/mocks/MockHooks.sol
+++ test/shared/mocks/MockHooks.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8.20;
 
 import 'src/access/IHooks.sol';
 
+event OnDepositCalled(address lender, uint256 scaledAmount, MarketState intermediateState, bytes extraData);
+event OnQueueWithdrawalCalled(address lender, uint scaledAmount, MarketState intermediateState, bytes extraData);
+event OnExecuteWithdrawalCalled(address lender, uint128 normalizedAmountWithdrawn, MarketState intermediateState, bytes extraData);
+event OnTransferCalled(address caller, address from, address to, uint scaledAmount, MarketState intermediateState, bytes extraData);
+event OnBorrowCalled(uint normalizedAmount, MarketState intermediateState, bytes extraData);
+event OnRepayCalled(uint normalizedAmount, MarketState intermediateState, bytes extraData);
+event OnCloseMarketCalled(MarketState intermediateState, bytes extraData);
+event OnAssetsSentToEscrowCalled(address lender, address asset, address escrow, uint scaledAmount, MarketState intermediateState, bytes extraData);
+event OnSetMaxTotalSupplyCalled(uint256 maxTotalSupply, MarketState intermediateState, bytes extraData);
+event OnSetAnnualInterestBipsCalled(uint16 annualInterestBips, MarketState intermediateState, bytes extraData);
+
 contract MockHooks is IHooks {
+  bytes32 public lastCalldataHash;
   HooksConfig public override config;
 
   /// @dev Returns the version string of the hooks contract.
@@ -26,21 +38,30 @@ contract MockHooks is IHooks {
     uint256 scaledAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnDepositCalled(lender, scaledAmount, intermediateState, extraData);
+  }
 
   function onQueueWithdrawal(
     address lender,
     uint scaledAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnQueueWithdrawalCalled(lender, scaledAmount, intermediateState, extraData);
+  }
 
   function onExecuteWithdrawal(
     address lender,
     uint128 normalizedAmountWithdrawn,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnExecuteWithdrawalCalled(lender, normalizedAmountWithdrawn, intermediateState, extraData);
+  }
 
   function onTransfer(
     address caller,
@@ -49,24 +70,36 @@ contract MockHooks is IHooks {
     uint scaledAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnTransferCalled(caller, from, to, scaledAmount, intermediateState, extraData);
+  }
 
   function onBorrow(
     uint normalizedAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnBorrowCalled(normalizedAmount, intermediateState, extraData);
+  }
 
   function onRepay(
     uint normalizedAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnRepayCalled(normalizedAmount, intermediateState, extraData);
+  }
 
   function onCloseMarket(
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnCloseMarketCalled(intermediateState, extraData);
+  }
 
   function onAssetsSentToEscrow(
     address lender,
@@ -75,17 +108,26 @@ contract MockHooks is IHooks {
     uint scaledAmount,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnAssetsSentToEscrowCalled(lender, asset, escrow, scaledAmount, intermediateState, extraData);
+  }
 
   function onSetMaxTotalSupply(
     uint256 maxTotalSupply,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnSetMaxTotalSupplyCalled(maxTotalSupply, intermediateState, extraData);
+  }
 
   function onSetAnnualInterestBips(
     uint16 annualInterestBips,
     MarketState calldata intermediateState,
     bytes calldata extraData
-  ) external virtual override {}
+  ) external virtual override {
+    lastCalldataHash = keccak256(msg.data);
+    emit OnSetAnnualInterestBipsCalled(annualInterestBips, intermediateState, extraData);
+  }
 }
--- test/types/HooksConfig.t.sol
+++ test/types/HooksConfig.t.sol
@@ -2,60 +2,61 @@
 pragma solidity ^0.8.20;
 
 import 'src/types/HooksConfig.sol';
-import 'forge-std/Test.sol';
+import { Test, console2 } from 'forge-std/Test.sol';
 import '../helpers/Assertions.sol';
 import '../shared/FuzzInputs.sol';
+import 'solady/utils/LibString.sol';
+import '../shared/mocks/MockHooks.sol';
+import '../shared/mocks/MockHookCaller.sol';
 
-contract MockHook {
-  bytes32 public lastCalldataHash;
-
-  fallback() external {}
-}
-
-contract MockHookCaller {
-  MockHook internal hooks = new MockHook();
-  MarketState internal state;
-
-  function setState(MarketState memory _state) external {
-    state = _state;
-  }
-
-  function deposit(uint256 scaledAmount) external {
-    hooks.onDeposit(msg.sender, scaledAmount, state, extraData);
-  }
-
-  function queueWithdrawal(uint scaledAmount) external {
-    hooks.onQueueWithdrawal(msg.sender, scaledAmount, state);
-  }
-
-  function executeWithdrawal(uint128 normalizedAmountWithdrawn) external {
-    hooks.onExecuteWithdrawal(msg.sender, normalizedAmountWithdrawn, state);
-  }
-
-  function transferFrom(address from, address to, uint scaledAmount) external {
-    hooks.onTransfer(msg.sender, from, to, scaledAmount, state, 0x64);
-  }
-
-  function transfer(address to, uint scaledAmount) external {
-    hooks.onTransfer(msg.sender, msg.sender, to, scaledAmount, state, 0x44);
-  }
-
-  function borrow(uint normalizedAmount) external {
-    hooks.onBorrow(normalizedAmount, state);
-  }
-
-  function repay(uint normalizedAmount) external {
-    hooks.onRepay(normalizedAmount, state, 0x24);
-  }
-
-  function closeMarket() external {
-    hooks.onCloseMarket(state);
-  }
-
-}
+using LibString for uint;
 
 contract HooksConfigTest is Test, Assertions {
-  MockHook internal hooks = new MockHook();
+  MockHooks internal hooks = new MockHooks();
+  MockHookCaller internal mockHookCaller = new MockHookCaller();
+
+  function _callMockHookCaller(bytes memory _calldata) internal {
+    assembly {
+      let success := call(
+        gas(),
+        sload(mockHookCaller.slot),
+        0,
+        add(_calldata, 0x20),
+        mload(_calldata),
+        0,
+        0
+      )
+      if iszero(success) {
+        returndatacopy(0, 0, returndatasize())
+        revert(0, returndatasize())
+      }
+    }
+  }
+
+  function prettyPrintBytes(bytes memory data, bool hasSelector) internal {
+    uint i;
+    if (hasSelector) {
+      i = 4;
+      uint selector;
+      assembly {
+        selector := shr(224, mload(add(data, 0x20)))
+      }
+      console2.log('[0:4]:', selector.toHexString());
+    }
+    for (; i < data.length; i += 32) {
+      uint word;
+      assembly {
+        word := mload(add(data, add(i, 0x20)))
+      }
+      uint end = i + 32;
+      if (end > data.length) {
+        end = data.length;
+      }
+      string memory prefix = string.concat('[', i.toString(), ':', end.toString(), ']: ');
+
+      console2.log(prefix, word.toHexString());
+    }
+  }
 
   function testEncode(StandardHooksConfig memory input) external {
     HooksConfig hooks = input.toHooksConfig();
@@ -64,91 +65,263 @@ contract HooksConfigTest is Test, Assertions {
 
   function test_onDeposit(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.deposit.selector, 100),
+      extraData
+    );
+    if (config.useOnDeposit()) {
+      vm.expectEmit();
+      emit OnDepositCalled(address(this), 100, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnDeposit()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onQueueWithdrawal(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.queueWithdrawal.selector, 100),
+      extraData
+    );
+    if (config.useOnQueueWithdrawal()) {
+      vm.expectEmit();
+      emit OnQueueWithdrawalCalled(address(this), 100, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnQueueWithdrawal()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onExecuteWithdrawal(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    address lender,
+    uint128 normalizedAmountWithdrawn,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(
+        mockHookCaller.executeWithdrawal.selector,
+        lender,
+        normalizedAmountWithdrawn
+      ),
+      extraData
+    );
+    if (config.useOnExecuteWithdrawal()) {
+      vm.expectEmit();
+      emit OnExecuteWithdrawalCalled(lender, normalizedAmountWithdrawn, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+
+    if (!config.useOnExecuteWithdrawal()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onTransfer(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    address to,
+    uint256 scaledAmount,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.transfer.selector, to, scaledAmount),
+      extraData
+    );
+
+    if (config.useOnTransfer()) {
+      vm.expectEmit();
+      emit OnTransferCalled(address(this), address(this), to, scaledAmount, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnTransfer()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onBorrow(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.borrow.selector, 100),
+      extraData
+    );
+    if (config.useOnBorrow()) {
+      vm.expectEmit();
+      emit OnBorrowCalled(100, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnBorrow()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onRepay(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.repay.selector, 100),
+      extraData
+    );
+    if (config.useOnRepay()) {
+      vm.expectEmit();
+      emit OnRepayCalled(100, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnRepay()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onCloseMarket(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.closeMarket.selector),
+      extraData
+    );
+    if (config.useOnCloseMarket()) {
+      vm.expectEmit();
+      emit OnCloseMarketCalled(state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnCloseMarket()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onAssetsSentToEscrow(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData,
+    address lender,
+    address asset,
+    address escrow,
+    uint scaledAmount
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(
+        mockHookCaller.sendAssetsToEscrow.selector,
+        lender,
+        asset,
+        escrow,
+        scaledAmount
+      ),
+      extraData
+    );
+    if (config.useOnAssetsSentToEscrow()) {
+      vm.expectEmit();
+      emit OnAssetsSentToEscrowCalled(lender, asset, escrow, scaledAmount, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnAssetsSentToEscrow()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onSetMaxTotalSupply(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.setMaxTotalSupply.selector, 100),
+      extraData
+    );
+    if (config.useOnSetMaxTotalSupply()) {
+      vm.expectEmit();
+      emit OnSetMaxTotalSupplyCalled(100, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnSetMaxTotalSupply()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 
   function test_onSetAnnualInterestBips(
     StateFuzzInputs memory stateInput,
-    StandardHooksConfig memory configInput
+    StandardHooksConfig memory configInput,
+    bytes memory extraData,
+    uint16 annualInterestBips
   ) external {
     MarketState memory state = stateInput.toState();
+    mockHookCaller.setState(state);
     configInput.hooksAddress = address(hooks);
     HooksConfig config = configInput.toHooksConfig();
+    mockHookCaller.setConfig(config);
+    bytes memory _calldata = abi.encodePacked(
+      abi.encodeWithSelector(mockHookCaller.setAnnualInterestBips.selector, annualInterestBips),
+      extraData
+    );
+    if (config.useOnSetAnnualInterestBips()) {
+      vm.expectEmit();
+      emit OnSetAnnualInterestBipsCalled(annualInterestBips, state, extraData);
+    }
+    _callMockHookCaller(_calldata);
+    if (!config.useOnSetAnnualInterestBips()) {
+      assertEq(hooks.lastCalldataHash(), 0);
+    }
   }
 }
