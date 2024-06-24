1260438fe3b58b7dc03b40b6d9be5f668c3b3331
d1ll0n <dillon@indexed.finance>
2024-06-24 05:50:04 -0700

    _blockAccount queues withdrawal instead of moving assets to escrow

--- src/market/WildcatMarket.sol
+++ src/market/WildcatMarket.sol
@@ -45,40 +45,36 @@ contract WildcatMarket is
     // Get current state
     MarketState memory state = _getUpdatedState();
 
-    if (_isSanctioned(msg.sender)) {
-      _blockAccount(state, msg.sender);
-      _writeState(state);
-    } else {
-      if (state.isClosed) revert_DepositToClosedMarket();
+    if (state.isClosed) revert_DepositToClosedMarket();
 
-      // Reduce amount if it would exceed totalSupply
-      amount = MathUtils.min(amount, state.maximumDeposit());
+    // Reduce amount if it would exceed totalSupply
+    amount = MathUtils.min(amount, state.maximumDeposit());
 
-      // Scale the mint amount
-      uint104 scaledAmount = state.scaleAmount(amount).toUint104();
-      if (scaledAmount == 0) revert_NullMintAmount();
+    // Scale the mint amount
+    uint104 scaledAmount = state.scaleAmount(amount).toUint104();
+    if (scaledAmount == 0) revert_NullMintAmount();
 
-      hooks.onDeposit(msg.sender, scaledAmount, state);
+    // Cache account data and revert if not authorized to deposit.
+    Account memory account = _getAccount(msg.sender);
 
-      // Transfer deposit from caller
-      asset.safeTransferFrom(msg.sender, address(this), amount);
+    hooks.onDeposit(msg.sender, scaledAmount, state);
 
-      // Cache account data and revert if not authorized to deposit.
-      Account memory account = _getAccount(msg.sender);
-      account.scaledBalance += scaledAmount;
-      _accounts[msg.sender] = account;
+    // Transfer deposit from caller
+    asset.safeTransferFrom(msg.sender, address(this), amount);
 
-      emit_Transfer(address(0), msg.sender, amount);
-      emit_Deposit(msg.sender, amount, scaledAmount);
+    account.scaledBalance += scaledAmount;
+    _accounts[msg.sender] = account;
 
-      // Increase supply
-      state.scaledTotalSupply += scaledAmount;
+    emit_Transfer(address(0), msg.sender, amount);
+    emit_Deposit(msg.sender, amount, scaledAmount);
 
-      // Update stored state
-      _writeState(state);
+    // Increase supply
+    state.scaledTotalSupply += scaledAmount;
 
-      return amount;
-    }
+    // Update stored state
+    _writeState(state);
+
+    return amount;
   }
 
   /**
@@ -272,9 +268,28 @@ contract WildcatMarket is
       revert_CloseMarketWithUnpaidWithdrawals();
     }
 
-    // @todo re-assess the order of operations here
-    // hooks.onCloseMarket(state);
     _writeState(state);
     emit_MarketClosed(block.timestamp);
+  }
+
+  /**
+   * @dev Queues a full withdrawal of a sanctioned account's assets.
+   */
+  function _blockAccount(MarketState memory state, address accountAddress) internal override {
+    Account memory account = _accounts[accountAddress];
+    if (account.scaledBalance > 0) {
+      uint104 scaledAmount = account.scaledBalance;
+
+      uint256 normalizedAmount = state.normalizeAmount(scaledAmount);
+
+      uint32 expiry = _queueWithdrawal(state, account, msg.sender, scaledAmount, normalizedAmount);
+
+      emit SanctionedAccountAssetsQueuedForWithdrawal(
+        expiry,
+        accountAddress,
+        scaledAmount,
+        normalizedAmount
+      );
+    }
   }
 }
