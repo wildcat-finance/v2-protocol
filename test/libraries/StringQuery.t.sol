5c2d8935047a45b29dfda45639f333308c5a510e
d1ll0n <dillon@wildcat.finance>
2024-08-02 23:47:01 -0700

    use LibERC20 for name and symbol tests

--- test/libraries/StringQuery.t.sol
+++ test/libraries/StringQuery.t.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.20;
 
 import 'forge-std/Test.sol';
 import 'src/libraries/StringQuery.sol';
+import 'src/libraries/LibERC20.sol';
 
 contract Bytes32Metadata {
   bytes32 public constant name = 'TestA';
@@ -12,6 +13,13 @@ contract Bytes32Metadata {
 contract StringMetadata {
   string public name = 'TestB';
   string public symbol = 'TestB';
+}
+
+contract LongStrings {
+  string public name =
+    'Wow this is such a long name you would never expect this to be used in a real token';
+  string public symbol =
+    'The symbol too? what is going on here? surely this is far too long for a ticker';
 }
 
 contract BadStrings {
@@ -28,26 +36,40 @@ contract BadStrings {
       revert();
     }
   }
+
+  function symbol() external {
+    if (giveRevertData) {
+      revert('symbol');
+    } else {
+      revert();
+    }
+  }
 }
 
 contract StringQueryTest is Test {
+  using LibERC20 for address;
   Bytes32Metadata internal immutable bytes32Metadata = new Bytes32Metadata();
   StringMetadata internal immutable stringMetadata = new StringMetadata();
+  LongStrings internal immutable longStrings = new LongStrings();
   BadStrings internal immutable badStrings = new BadStrings();
 
   function queryName(address token) external view returns (string memory) {
-    return queryName(token);
+    return token.name();
   }
 
   function querySymbol(address token) external view returns (string memory) {
-    return querySymbol(token);
+    return token.symbol();
   }
 
-  function test_queryName() external {
-    assertEq(queryName(address(bytes32Metadata)), 'TestA');
-    assertEq(queryName(address(stringMetadata)), 'TestB');
+  function test_name() external {
+    assertEq(address(bytes32Metadata).name(), 'TestA');
+    assertEq(address(stringMetadata).name(), 'TestB');
+    assertEq(
+      address(longStrings).name(),
+      'Wow this is such a long name you would never expect this to be used in a real token'
+    );
 
-    vm.expectRevert(bytes4(bytes32(UnknownNameQueryError_selector)));
+    vm.expectRevert(LibERC20.NameFailed.selector);
     this.queryName(address(badStrings));
 
     badStrings.setGiveRevertData(true);
@@ -55,8 +77,19 @@ contract StringQueryTest is Test {
     this.queryName(address(badStrings));
   }
 
-  function test_querySymbol() external {
-    assertEq(querySymbol(address(bytes32Metadata)), 'TestA');
-    assertEq(querySymbol(address(stringMetadata)), 'TestB');
+  function test_symbol() external {
+    assertEq(address(bytes32Metadata).symbol(), 'TestA');
+    assertEq(address(stringMetadata).symbol(), 'TestB');
+    assertEq(
+      address(longStrings).symbol(),
+      'The symbol too? what is going on here? surely this is far too long for a ticker'
+    );
+
+    vm.expectRevert(LibERC20.SymbolFailed.selector);
+    this.querySymbol(address(badStrings));
+
+    badStrings.setGiveRevertData(true);
+    vm.expectRevert(bytes('symbol'));
+    this.querySymbol(address(badStrings));
   }
 }
