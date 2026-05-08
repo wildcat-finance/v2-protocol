57b4d703fd8856d65297b6fdf574bccd5bad1362
Dave Coleman <dave@wildcat.finance>
2026-05-08 14:47:34 -0700

    CAF-21: round to zero parametric sweep test
    Signed-off-by: Dave Coleman <dave@wildcat.finance>

--- test/market/WildcatMarketRevolving.t.sol
+++ test/market/WildcatMarketRevolving.t.sol
@@ -26,6 +26,7 @@ contract WildcatMarketRevolvingTest is Test {
     address internal hooksInstance;
     WildcatMarket internal market;
     IWildcatMarketRevolving internal revolvingMarket;
+    uint256 internal nextScenarioSalt = 2;
 
     address internal borrower;
     address internal constant lender = address(0xA11CE);
@@ -88,6 +89,162 @@ contract WildcatMarketRevolvingTest is Test {
     function _deposit(address account, uint256 amount) internal {
         vm.prank(account);
         market.depositUpTo(amount);
+    }
+
+    function _deposit(WildcatMarket targetMarket, MockERC20 targetUnderlying, address account, uint256 amount) internal {
+        targetUnderlying.mint(account, amount);
+        vm.startPrank(account);
+        targetUnderlying.approve(address(targetMarket), amount);
+        targetMarket.depositUpTo(amount);
+        vm.stopPrank();
+    }
+
+    function _deployRevolvingMarket(
+        MockERC20 targetUnderlying,
+        uint128 maxTotalSupply,
+        uint16 targetAnnualInterestBips,
+        uint16 targetCommitmentFeeBips
+    ) internal returns (WildcatMarket targetMarket, IWildcatMarketRevolving targetRevolvingMarket) {
+        address targetHooksInstance = hooksFactoryRevolving.deployHooksInstance(hooksTemplate, bytes(""));
+        DeployMarketInputs memory params = DeployMarketInputs({
+            asset: address(targetUnderlying),
+            namePrefix: "Wildcat ",
+            symbolPrefix: "wc",
+            maxTotalSupply: maxTotalSupply,
+            annualInterestBips: targetAnnualInterestBips,
+            delinquencyFeeBips: 0,
+            withdrawalBatchDuration: 1 days,
+            reserveRatioBips: 2_000,
+            delinquencyGracePeriod: 1 days,
+            hooks: EmptyHooksConfig.setHooksAddress(targetHooksInstance)
+        });
+        address marketAddress = hooksFactoryRevolving.deployMarket(
+            params,
+            bytes(""),
+            abi.encode(uint8(1), targetCommitmentFeeBips),
+            bytes32(nextScenarioSalt++),
+            address(0),
+            0
+        );
+        targetMarket = WildcatMarket(marketAddress);
+        targetRevolvingMarket = IWildcatMarketRevolving(marketAddress);
+    }
+
+    function _deployFundedRevolvingMarket(
+        uint8 decimals,
+        uint128 totalSupply,
+        uint16 targetAnnualInterestBips
+    ) internal returns (WildcatMarket targetMarket, IWildcatMarketRevolving targetRevolvingMarket) {
+        MockERC20 targetUnderlying = new MockERC20("Scenario Underlying", "SCN", decimals);
+        (targetMarket, targetRevolvingMarket) = _deployRevolvingMarket(
+            targetUnderlying,
+            totalSupply,
+            targetAnnualInterestBips,
+            0
+        );
+        _deposit(targetMarket, targetUnderlying, lender, totalSupply);
+    }
+
+    function _utilizationInterestRay(
+        uint256 totalSupply,
+        uint256 drawn,
+        uint16 targetAnnualInterestBips,
+        uint32 updateInterval
+    ) internal pure returns (uint256) {
+        uint256 annualInterestRay = MathUtils.calculateLinearInterestFromBips(
+            targetAnnualInterestBips,
+            updateInterval
+        );
+        return MathUtils.mulDiv(annualInterestRay, drawn, totalSupply);
+    }
+
+    function _minDrawForNonzeroUtilizationInterest(
+        uint256 totalSupply,
+        uint16 targetAnnualInterestBips,
+        uint32 updateInterval
+    ) internal pure returns (uint256) {
+        uint256 annualInterestRay = MathUtils.calculateLinearInterestFromBips(
+            targetAnnualInterestBips,
+            updateInterval
+        );
+        return (totalSupply / annualInterestRay) + 1;
+    }
+
+    function _assertObservedMarketAccruesForOneBaseUnit(
+        uint128 totalSupply,
+        uint16 targetAnnualInterestBips,
+        uint32 updateInterval
+    ) internal {
+        uint256 minDraw = _minDrawForNonzeroUtilizationInterest(
+            totalSupply,
+            targetAnnualInterestBips,
+            updateInterval
+        );
+        assertEq(minDraw, 1, "one base unit should be enough for observed 6-decimal size");
+
+        uint256 expectedUtilizationInterestRay = _utilizationInterestRay(
+            totalSupply,
+            1,
+            targetAnnualInterestBips,
+            updateInterval
+        );
+        assertGt(expectedUtilizationInterestRay, 0, "one base unit should accrue utilization interest");
+
+        (WildcatMarket targetMarket,) = _deployFundedRevolvingMarket(
+            6,
+            totalSupply,
+            targetAnnualInterestBips
+        );
+        targetMarket.borrow(1);
+
+        vm.warp(block.timestamp + updateInterval);
+        targetMarket.updateState();
+
+        assertEq(
+            targetMarket.scaleFactor(),
+            RAY + expectedUtilizationInterestRay,
+            "observed-size utilization interest"
+        );
+    }
+
+    function _assertUtilizationDustBoundary(
+        uint128 totalSupply,
+        uint16 targetAnnualInterestBips,
+        uint32 updateInterval
+    ) internal {
+        uint256 minDraw = _minDrawForNonzeroUtilizationInterest(
+            totalSupply,
+            targetAnnualInterestBips,
+            updateInterval
+        );
+        assertGt(minDraw, 1, "test case must have a below-threshold draw");
+        assertLt(minDraw, totalSupply, "threshold must be borrowable");
+        assertEq(
+            _utilizationInterestRay(totalSupply, minDraw - 1, targetAnnualInterestBips, updateInterval),
+            0,
+            "draw below threshold should truncate"
+        );
+        assertEq(
+            _utilizationInterestRay(totalSupply, minDraw, targetAnnualInterestBips, updateInterval),
+            1,
+            "threshold draw should accrue one ray unit"
+        );
+
+        (WildcatMarket belowThresholdMarket,) = _deployFundedRevolvingMarket(
+            18,
+            totalSupply,
+            targetAnnualInterestBips
+        );
+        belowThresholdMarket.borrow(minDraw - 1);
+        vm.warp(block.timestamp + updateInterval);
+        belowThresholdMarket.updateState();
+        assertEq(belowThresholdMarket.scaleFactor(), RAY, "below threshold scale factor");
+
+        (WildcatMarket thresholdMarket,) = _deployFundedRevolvingMarket(18, totalSupply, targetAnnualInterestBips);
+        thresholdMarket.borrow(minDraw);
+        vm.warp(block.timestamp + updateInterval);
+        thresholdMarket.updateState();
+        assertEq(thresholdMarket.scaleFactor(), RAY + 1, "threshold scale factor");
     }
 
     function _accruedDebtAboveDrawn() internal view returns (uint256) {
@@ -201,6 +358,48 @@ contract WildcatMarketRevolvingTest is Test {
 
         uint256 expectedScaleFactor = RAY + commitmentInterestRay + utilizationInterestRay;
         assertEq(market.scaleFactor(), expectedScaleFactor);
+    }
+
+    function test_updateState_utilizationDustThresholds_matchObservedAndStressBallpark() external {
+        assertEq(_minDrawForNonzeroUtilizationInterest(1_000e6, 1, 12), 1, "1k USDC, 1 bip, 12s");
+        assertEq(
+            _minDrawForNonzeroUtilizationInterest(110_000_000e6, 1, 12),
+            1,
+            "110m USDC, 1 bip, 12s"
+        );
+        assertEq(
+            _minDrawForNonzeroUtilizationInterest(110_000_000e6, 1, 1),
+            1,
+            "110m USDC, 1 bip, 1s"
+        );
+        assertEq(
+            _minDrawForNonzeroUtilizationInterest(110_000_000e18, 1, 12),
+            2_890_800_001,
+            "110m 18-dec, 1 bip, 12s"
+        );
+        assertEq(
+            _minDrawForNonzeroUtilizationInterest(110_000_000e18, 1_000, 12),
+            2_890_801,
+            "110m 18-dec, 1000 bips, 12s"
+        );
+        assertEq(
+            _minDrawForNonzeroUtilizationInterest(110_000_000e18, 1, 1),
+            34_689_600_001,
+            "110m 18-dec, 1 bip, 1s"
+        );
+    }
+
+    function test_updateState_utilizationDustThresholds_observedUsdcMarkets() external {
+        _assertObservedMarketAccruesForOneBaseUnit(1_000e6, 1, 12);
+        _assertObservedMarketAccruesForOneBaseUnit(110_000_000e6, 1, 12);
+        _assertObservedMarketAccruesForOneBaseUnit(110_000_000e6, 1_000, 12);
+    }
+
+    function test_updateState_utilizationDustBoundary_eighteenDecimalStressCases() external {
+        _assertUtilizationDustBoundary(1_000e18, 1, 12);
+        _assertUtilizationDustBoundary(1_000e18, 1_000, 12);
+        _assertUtilizationDustBoundary(110_000_000e18, 1, 12);
+        _assertUtilizationDustBoundary(110_000_000e18, 1_000, 12);
     }
 
     function test_updateState_zeroTimeDelta_doesNotAccrueInterest() external {
